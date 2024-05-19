7f3e8c3b7a0af81429329610beb39af64a876272
QIHUAN1 <71250585+QIHUAN1@users.noreply.github.com>
2024-05-19 14:04:20 -0700

    start and end
    1

---
+++ PixelArtJam/Assets/Script/Qihuan Scipt/Menu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+}
