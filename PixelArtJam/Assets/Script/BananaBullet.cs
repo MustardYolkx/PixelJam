c2ed286c60a30afb1548fb8b9438f723d2c5891d
MustardYolkxxxx <161983067+MustardYolkx@users.noreply.github.com>
2024-05-20 13:23:21 +0800

    Import Enmey anim

--- PixelArtJam/Assets/Script/BananaBullet.cs
+++ PixelArtJam/Assets/Script/BananaBullet.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BananaBullet : MonoBehaviour
 {
     public float damage;
     public float hitForce;
+    /*[HideInInspector] */public bool isOnground = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isOnground = false;
     }
 
     // Update is called once per frame
@@ -27,6 +29,27 @@ public class BananaBullet : MonoBehaviour
             player.TakeDamage(damage);
             player.rb.AddForce(direction * hitForce, ForceMode2D.Force);
             Destroy(gameObject);
+        }
+
+        GroundCheck ground = collision.GetComponent<GroundCheck>();
+        if (ground != null)
+        {
+            isOnground = true;
+        }
+
+
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        OceanDamage ocean = collision.GetComponent<OceanDamage>();
+       
+        if (ocean != null)
+        {
+            if (!isOnground)
+            {
+                Destroy(gameObject);
+            }
+
         }
     }
 }
